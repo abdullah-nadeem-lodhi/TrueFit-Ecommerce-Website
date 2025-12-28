c3f59e5112d915c431cf9c808577ffd59c32eb10
Ayesha Ibrahim <ashuchaudhry005@gmail.com>
2025-12-29 04:08:21 +0500

    ProductDisplay done . next step is to make description box

--- Front End/Ecommerce Website/src/Components/Items/Items.jsx
+++ Front End/Ecommerce Website/src/Components/Items/Items.jsx
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom";
 const Items = (props) => {
   return (
     <div className="item">
-        <Link to={'/product/${props.id}'}><img src={props.image} alt={props.name} /> </Link>
+       <Link to={`/product/${props.id}`}><img src={props.image} alt={props.name} /> </Link>
+
         <p>{props.name}</p>
         <div className="item-prices">
 
