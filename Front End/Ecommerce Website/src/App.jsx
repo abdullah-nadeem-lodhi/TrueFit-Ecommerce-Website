3d67ca088b0b3d6fe80e394753c2c5bc874ef591
Ayesha Ibrahim <ashuchaudhry005@gmail.com>
2025-12-28 16:15:09 +0500

    Footer  created next step is to make shop context

--- Front End/Ecommerce Website/src/App.jsx
+++ Front End/Ecommerce Website/src/App.jsx
@@ -6,6 +6,7 @@ import LoginSignup from './Pages/LoginSignup.jsx'
 import Shop from './Pages/Shop.jsx'
 import Product from './Pages/Product.jsx'
 import Cart from './Pages/Cart.jsx'
+import Footer from './Components/Footer/Footer.jsx'
 
 function App() {
 
@@ -24,6 +25,8 @@ function App() {
       <Route path="/cart" element={<Cart/>} />
       <Route path="/login" element={<LoginSignup />} />
     </Routes>
+    <Footer />
+    
     </BrowserRouter>
     </>
   )
