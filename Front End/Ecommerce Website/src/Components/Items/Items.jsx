86395245fbb55c034946ef91281ff86e0a094b1e
abdullah-nadeem-lodhi <abdullahnadeemlodhi@gmail.com>
2025-12-29 18:33:55 +0500

    Item Image Scaling on hover fixed as per required. Now Working on the media queries in it for responsiveness

--- Front End/Ecommerce Website/src/Components/Items/Items.jsx
+++ Front End/Ecommerce Website/src/Components/Items/Items.jsx
@@ -5,14 +5,17 @@ import { Link } from "react-router-dom";
 const Items = (props) => {
   return (
     <div className="item">
-       <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0, 0)} src={props.image} alt={props.name} /> </Link>
+       <Link to={`/product/${props.id}`} onClick={() => window.scrollTo(0, 0)}>
+         <div className="item-img-container">
+           <img src={props.image} alt={props.name} />
+         </div>
+       </Link>
 
         <p>{props.name}</p>
         <div className="item-prices">
-
+            <div className="item-price-new">${props.new_price}</div>
+            <div className="item-price-old">${props.old_price}</div>
         </div>
-        <div className="item-price-new">${props.new_price}</div>
-        <div className="item-price-old">${props.old_price}</div>
     </div>
   );
 }
