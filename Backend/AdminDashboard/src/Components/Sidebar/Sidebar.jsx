bcc6c52bf634432e85ea57e30a70acb238ffb504
abdullah-nadeem-lodhi <abdullahnadeemlodhi@gmail.com>
2026-01-11 00:11:12 +0500

    Navbar Complete. Next Step to Apply CSS on Sidebar and complete it

---
+++ Backend/AdminDashboard/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Sidebar.css'
+import {Link} from 'react-router-dom'
+import addProductIcon from '../../../../../Assets/Admin_Assets/Product_Cart.svg'
+import listProductIcon from '../../../../../Assets/Admin_Assets/Product_list_icon.svg'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <Link to={'/addproduct'} style={{textDecoration:"none"}}>
+        <div className="sidebar-item">
+            <img src={addProductIcon} alt="" />
+            <p>Add Product</p>
+        </div>
+        </Link>
+        <Link to={'/listproduct'} style={{textDecoration:"none"}}>
+        <div className="sidebar-item">
+            <img src={listProductIcon} alt="" />
+            <p>Product List</p>
+        </div>
+        </Link>
+      
+    </div>
+  )
+}
+
+export default Sidebar
